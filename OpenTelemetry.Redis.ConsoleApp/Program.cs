183c1886a14fd90d40ec9dab3dc86178fa6db9f6
Matt Hughes <mhughes@cardlytics.com>
2020-11-16 13:09:01 -0500

    Add app to demonstrate StackExchange.Redis OpenTelemetry data writing to console

---
+++ OpenTelemetry.Redis.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using OpenTelemetry.Trace;
+using StackExchange.Redis;
+
+namespace OpenTelemetry.Redis.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = ConnectionMultiplexer.Connect("localhost:6379");
+
+            Sdk.CreateTracerProviderBuilder()
+                .AddRedisInstrumentation(connection, opt => opt.FlushInterval = TimeSpan.FromSeconds(1))
+                .AddConsoleExporter()
+                .Build();
+
+            var db = connection.GetDatabase();
+
+            var msg = db.StringGet("testKey01");
+            Console.WriteLine(msg);
+        }
+    }
+}
